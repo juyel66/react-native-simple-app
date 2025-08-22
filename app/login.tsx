2791285eed6fd17516c7de5ab2603677ae12b19d
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-08-23 02:49:39 +0600

    native app

---
+++ app/login.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from "react-native"
+
+export default function LoginScreen() {
+    return (
+        <View>
+            <Text>
+                This is a login page 
+
+            </Text>
+        </View>
+    )
+}
