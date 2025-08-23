59075baa21f80bbea87274b39f3ef66ca56b6941
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-08-23 06:26:43 +0600

    chnage home button style

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
 import {Tabs } from "expo-router";
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 
 export default function TabsLayout() {
@@ -9,9 +10,14 @@ export default function TabsLayout() {
    
      <Tabs screenOptions={{tabBarActiveTintColor: "coral"}}>
      
-      <Tabs.Screen name="index" options={{title: 'Home', tabBarIcon: () => (
-         <FontAwesome name="home" size={24} color="black" />
-      ) }} />
+      <Tabs.Screen name="index" options={{title: 'Home', tabBarIcon: ({color, focused}) => {
+        return focused ? (
+
+          
+          <FontAwesome name="home" size={24} color={color} />
+        ) :
+        <AntDesign name="home" size={24} color="black" />
+      } }} />
       <Tabs.Screen name="login" options={{title: 'Login'}} />
      </Tabs>
    
